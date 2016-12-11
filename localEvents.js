9d699d43f0a433fb0525215b1faf53fd9e408563
benjamin99 <cknomber1@gmail.com>
2016-12-11 12:10:53 +0800

    Added the crud basic implementations for local events

---
+++ localEvents.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const Promise = require('bluebird');
+const Joi = Promise.promisifyAll(require('joi'));
+const crud = require('./crudHandlers');
+const utils = require('./utils');
+const ERROR_CODE = utils.ERROR_CODE;
+const render = utils.render;
+const handleError = utils.handleError;
+
+const table = 'local-events';
+
+/** member CRUD implementations */
+
+const create = Promise.promisify(crud.createHandler(table));
+const list = Promise.promisify(crud.listHandler(table));
+const show = Promise.promisify(crud.showHandler(table));
+const update = Promise.promisify(crud.updateHandler(table));
+const destroy = Promise.promisify(crud.destroyHandler(table));
+
+/** Joi schemas */
+
+const createSchema = Joi.object().keys({
+  title: Joi.string(),
+  content: Joi.string(),
+  imageUrl: Joi.string(),
+  channelId: Joi.string()
+}).requiredKeys('title', 'content');
+
+/** lambda function implementations */
+
+const headers = { 'Access-Control-Allow-Origin': '*' };
+
+module.exports.create = (event, context, callback) => {
+  Joi.validateAsync(event.body, createSchema)
+    .then(form => { return create(event); })
+    .then(result => render(context, 201, headers, result))
+    .catch(error => handleError(context, headers, error));
+};
+
+module.exports.list = (event, context, callback) => {
+  list(event)
+    .then(result => render(context, 200, headers, result))
+    .catch(error => handleError(context, headers, error));
+};
+
+module.exports.show = (event, context, callback) => {
+  show(event).then(item => {
+    const status = item ? 200 : 404
+    const body = item ? item : {
+      error: ERROR_CODE.eventNotFound,
+      message: 'event not found'
+    };
+
+    render(context, status, headers, body);
+  }).catch(error => handleError(context, headers, error));
+};
+
+module.exports.update = (event, context, callback) => {
+  update(event)
+    .then(result => render(context, 200, headers, result))
+    .catch(error => handleError(context, headers, error));
+};
+
+module.exports.destroy = (event, context, callback) => {
+  destroy(event).then(result => {
+    if (!result.Attributes) {
+      return render(context, 404, headers, {
+        error: ERROR_CODE.eventNotFound,
+        message: 'event not found'
+      });
+    }
+
+    render(context, 200, headers, { id: result.Attributes.id });
+
+  }).catch(error => handleError(context, headers, error));
+};
